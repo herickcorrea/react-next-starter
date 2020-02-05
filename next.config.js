7a015d38be6c239480898d65c5d21d1bc9f275fa
Herick Correa <hcorrea@gm5.com.br>
2020-02-05 13:09:56 -0300

    Habilitação de importação de arquivos estáticos, como imagens

--- next.config.js
+++ next.config.js
@@ -1,24 +1,25 @@
 const withCss = require('@zeit/next-css');
 const withLess = require('@zeit/next-less');
+// const withImages = require('next-images');
 
-const withCSS = require("@zeit/next-css");
-module.exports = withCSS(withLess({
-    webpack(config, options) 
+module.exports = withCss(withLess(
     {
+        webpack(config, options) 
+        {
+            config.module.rules.push({
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        limit: 100000,
+                        name: '[path][name].[ext]?[contenthash]',
+                        publicPath: '/_next/static',
+                        outputPath: "static"
+                    }
+                },
+            });
 
-        config.module.rules.push({
-            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-            use: {
-                loader: 'file-loader',
-                options: {
-                    limit: 100000,
-                    name: '[name].[ext]?[contenthash]',
-                    publicPath: '/_next/static',
-                    outputPath: "static"
-                }
-            },
-        });
-
-        return config;
+            return config;
+        }
     }
-}));
+));
